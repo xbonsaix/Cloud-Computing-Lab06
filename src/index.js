67fb93e9b37ec0a11cd3da06990f080d705747ec
Chea Chansoksambath <cheasambath40@yahoo,com>
2021-03-26 10:58:21 +0700

    error on line 21 index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ db.init().then(() => {
     app.listen(3000, () => console.log('Listening on port 3000'));
 }).catch((err) => {
     console.error(err);
-    process.exit(1);
+    process.exitttttttttttttt(1);
 });
 
 // const gracefulShutdown = () => {
